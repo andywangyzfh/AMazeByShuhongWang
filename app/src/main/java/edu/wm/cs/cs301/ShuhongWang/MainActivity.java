5762e0b349ccd4ceb191a3cdbea4d24c26ec0d6c
ShuhongWangWM <swang32@email.wm.edu>
2020-10-24 16:42:19 +0800

    Initial commit

---
+++ app/src/main/java/edu/wm/cs/cs301/ShuhongWang/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.wm.cs.cs301.ShuhongWang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
